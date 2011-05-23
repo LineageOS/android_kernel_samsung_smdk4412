8c3115524089d3fc9f8071df5f1c9dd568659a5d
Paul Gortmaker <paul.gortmaker@windriver.com>
2011-05-23 14:51:41 -0400

    kernel: Map most files to use export.h instead of module.h
    The changed files were only including linux/module.h for the
    EXPORT_SYMBOL infrastructure, and nothing else.  Revector them
    onto the isolated export header for faster compile times.
    
    Nothing to see here but a whole lot of instances of:
    
      -#include <linux/module.h>
      +#include <linux/export.h>
    
    This commit is only changing the kernel dir; next targets
    will probably be mm, fs, the arch dirs, etc.
    
    Change-Id: I73e614a96ff812955f038bc056bad6eb1f17d3f3
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- kernel/async.c
+++ kernel/async.c
@@ -49,7 +49,9 @@ asynchronous and synchronous parts of the kernel.
 */
 
 #include <linux/async.h>
-#include <linux/module.h>
+#include <linux/atomic.h>
+#include <linux/ktime.h>
+#include <linux/export.h>
 #include <linux/wait.h>
 #include <linux/sched.h>
 #include <linux/slab.h>
