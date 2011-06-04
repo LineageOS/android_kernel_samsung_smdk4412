dd6f32168fb05dda3078d85b1b98e3ec82d5137c
Al Viro <viro@zeniv.linux.org.uk>
2011-06-03 20:16:57 -0400

    new helper: iterate_supers_type()
    Call the given function for all superblocks of given type.  Function
    gets a superblock (with s_umount locked shared) and (void *) argument
    supplied by caller of iterator.
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -2483,6 +2483,8 @@ extern struct super_block *get_super(struct block_device *);
 extern struct super_block *get_active_super(struct block_device *bdev);
 extern void drop_super(struct super_block *sb);
 extern void iterate_supers(void (*)(struct super_block *, void *), void *);
+extern void iterate_supers_type(struct file_system_type *,
+			        void (*)(struct super_block *, void *), void *);
 
 extern int dcache_dir_open(struct inode *, struct file *);
 extern int dcache_dir_close(struct inode *, struct file *);
