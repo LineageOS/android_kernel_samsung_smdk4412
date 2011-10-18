3d7c233048068ac1bf87545c7792a6836bd7281c
H Hartley Sweeten <hartleys@visionengravers.com>
2011-10-18 10:59:51 -0400

    ext4: add __user decoration to calls of copy_{from,to}_user()
    This quiets the sparse noise:
    
    warning: incorrect type in argument 2 (different address spaces)
       expected void const [noderef] <asn:1>*from
       got struct fstrim_range *<noident>
    warning: incorrect type in argument 1 (different address spaces)
       expected void [noderef] <asn:1>*to
       got struct fstrim_range *<noident>
    
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Signed-off-by: "Theodore Ts'o" <tytso@mit.edu>

--- fs/ext4/ioctl.c
+++ fs/ext4/ioctl.c
@@ -378,7 +378,7 @@ mext_out:
 			return -EOPNOTSUPP;
 		}
 
-		if (copy_from_user(&range, (struct fstrim_range *)arg,
+		if (copy_from_user(&range, (struct fstrim_range __user *)arg,
 		    sizeof(range)))
 			return -EFAULT;
 
@@ -388,7 +388,7 @@ mext_out:
 		if (ret < 0)
 			return ret;
 
-		if (copy_to_user((struct fstrim_range *)arg, &range,
+		if (copy_to_user((struct fstrim_range __user *)arg, &range,
 		    sizeof(range)))
 			return -EFAULT;
 
