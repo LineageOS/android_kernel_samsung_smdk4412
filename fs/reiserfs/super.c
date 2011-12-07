10d8415e530dd5425e2b26a3d13eeb8b24b711d5
Al Viro <viro@zeniv.linux.org.uk>
2011-12-07 18:16:57 -0500

    vfs: prefer ->dentry->d_sb to ->mnt->mnt_sb
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/reiserfs/super.c
+++ fs/reiserfs/super.c
@@ -2089,7 +2089,7 @@ static int reiserfs_quota_on(struct super_block *sb, int type, int format_id,
 	}
 
 	/* Quotafile not on the same filesystem? */
-	if (path->mnt->mnt_sb != sb) {
+	if (path->dentry->d_sb != sb) {
 		err = -EXDEV;
 		goto out;
 	}
