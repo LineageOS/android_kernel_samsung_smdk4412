eba60b74913333cecb1cdba468d9b072d6f57b79
Dan Magenheimer <dan.magenheimer@oracle.com>
2012-01-25 16:58:46 -0800

    mm: implement WasActive page flag (for improving cleancache)
    (Feedback welcome if there is a different/better way to do this
    without using a page flag!)
    
    Since about 2.6.27, the page replacement algorithm maintains
    an "active" bit to help decide which pages are most eligible
    to reclaim, see http://linux-mm.org/PageReplacementDesign
    
    This "active' information is also useful to cleancache but is lost
    by the time that cleancache has the opportunity to preserve the
    pageful of data.  This patch adds a new page flag "WasActive" to
    retain the state.  The flag may possibly be useful elsewhere.
    
    It is up to each cleancache backend to utilize the bit as
    it desires.  The matching patch for zcache is included here
    for clarification/discussion purposes, though it will need to
    go through GregKH and the staging tree.
    
    The patch resolves issues reported with cleancache which occur
    especially during streaming workloads on older processors,
    see https://lkml.org/lkml/2011/8/17/351
    
    Signed-off-by: Dan Magenheimer <dan.magenheimer@oracle.com>
    
    Conflicts:
    	include/linux/page-flags.h
    
    Change-Id: I0fcb2302a7b9c5e66db005229f679baee90f262f
    
    Conflicts:
    	include/linux/page-flags.h

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -651,6 +651,8 @@ void putback_lru_page(struct page *page)
 	int was_unevictable = PageUnevictable(page);
 
 	VM_BUG_ON(PageLRU(page));
+	if (active)
+		SetPageWasActive(page);
 
 redo:
 	ClearPageUnevictable(page);
@@ -1334,6 +1336,7 @@ unsigned long clear_active_flags(struct list_head *page_list,
 		if (PageActive(page)) {
 			lru += LRU_ACTIVE;
 			ClearPageActive(page);
+			SetPageWasActive(page);
 			nr_active += numpages;
 		}
 		if (count)
@@ -1755,6 +1758,7 @@ static void shrink_active_list(unsigned long nr_pages, struct zone *zone,
 		}
 
 		ClearPageActive(page);	/* we are de-activating */
+		SetPageWasActive(page);
 		list_add(&page->lru, &l_inactive);
 	}
 
