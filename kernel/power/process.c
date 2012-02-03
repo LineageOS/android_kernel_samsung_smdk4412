7c7c13295108c40fb18e0aa85743a060a47fdc85
Srivatsa S. Bhat <srivatsa.bhat@linux.vnet.ibm.com>
2012-02-03 22:22:41 +0100

    PM / Freezer: Thaw only kernel threads if freezing of kernel threads fails
    If freezing of kernel threads fails, we are expected to automatically
    thaw tasks in the error recovery path. However, at times, we encounter
    situations in which we would like the automatic error recovery path
    to thaw only the kernel threads, because we want to be able to do
    some more cleanup before we thaw userspace. Something like:
    
    error = freeze_kernel_threads();
    if (error) {
    	/* Do some cleanup */
    
    	/* Only then thaw userspace tasks*/
    	thaw_processes();
    }
    
    An example of such a situation is where we freeze/thaw filesystems
    during suspend/hibernation. There, if freezing of kernel threads
    fails, we would like to thaw the frozen filesystems before thawing
    the userspace tasks.
    
    So, modify freeze_kernel_threads() to thaw only kernel threads in
    case of freezing failure. And change suspend_freeze_processes()
    accordingly. (At the same time, let us also get rid of the rather
    cryptic usage of the conditional operator (:?) in that function.)
    
    [rjw: In fact, this patch fixes a regression introduced during the
     3.3 merge window, because without it thaw_processes() may be called
     before swsusp_free() in some situations and that may lead to massive
     memory allocation failures.]
    
    Signed-off-by: Srivatsa S. Bhat <srivatsa.bhat@linux.vnet.ibm.com>
    Acked-by: Tejun Heo <tj@kernel.org>
    Acked-by: Nigel Cunningham <nigel@tuxonice.net>
    Signed-off-by: Rafael J. Wysocki <rjw@sisk.pl>

--- kernel/power/process.c
+++ kernel/power/process.c
@@ -148,7 +148,10 @@ int freeze_processes(void)
 /**
  * freeze_kernel_threads - Make freezable kernel threads go to the refrigerator.
  *
- * On success, returns 0.  On failure, -errno and system is fully thawed.
+ * On success, returns 0.  On failure, -errno and only the kernel threads are
+ * thawed, so as to give a chance to the caller to do additional cleanups
+ * (if any) before thawing the userspace tasks. So, it is the responsibility
+ * of the caller to thaw the userspace tasks, when the time is right.
  */
 int freeze_kernel_threads(void)
 {
@@ -164,7 +167,7 @@ int freeze_kernel_threads(void)
 	BUG_ON(in_atomic());
 
 	if (error)
-		thaw_processes();
+		thaw_kernel_threads();
 	return error;
 }
 
