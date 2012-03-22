924011da29a475922768b08018f730c8edd81771
Linus Torvalds <torvalds@linux-foundation.org>
2012-03-22 16:10:40 -0700

    vfs: tidy up sparse warnings in fs/namei.c
    While doing the fs/namei.c cleanups, I ran sparse on it, and it pointed
    out other large integers and a couple of cases of us using '0' instead
    of the proper 'NULL'.
    
    Sparse still doesn't understand some of the conditional locking going
    on, but that's no excuse for not fixing up the trivial stuff.
    
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- fs/namei.c
+++ fs/namei.c
@@ -160,7 +160,7 @@ static char *getname_flags(const char __user *filename, int flags, int *empty)
 
 char *getname(const char __user * filename)
 {
-	return getname_flags(filename, 0, 0);
+	return getname_flags(filename, 0, NULL);
 }
 
 #ifdef CONFIG_AUDITSYSCALL
@@ -1370,7 +1370,7 @@ static inline int can_lookup(struct inode *inode)
  */
 static inline long count_masked_bytes(unsigned long mask)
 {
-	return mask*0x0001020304050608 >> 56;
+	return mask*0x0001020304050608ul >> 56;
 }
 
 static inline unsigned int fold_hash(unsigned long hash)
@@ -1957,7 +1957,7 @@ int user_path_at_empty(int dfd, const char __user *name, unsigned flags,
 int user_path_at(int dfd, const char __user *name, unsigned flags,
 		 struct path *path)
 {
-	return user_path_at_empty(dfd, name, flags, path, 0);
+	return user_path_at_empty(dfd, name, flags, path, NULL);
 }
 
 static int user_path_parent(int dfd, const char __user *path,
