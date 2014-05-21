a0d7e010119ade3586b4fbad2e4f54ea8edd5f5b
Kees Cook <keescook@chromium.org>
2014-05-21 15:23:46 -0700

    sched: move no_new_privs into new atomic flags
    Since seccomp transitions between threads requires updates to the
    no_new_privs flag to be atomic, the flag must be part of an atomic flag
    set. This moves the nnp flag into a separate task field, and introduces
    accessors.
    
    Signed-off-by: Kees Cook <keescook@chromium.org>
    Reviewed-by: Oleg Nesterov <oleg@redhat.com>
    Reviewed-by: Andy Lutomirski <luto@amacapital.net>
    
    Conflicts:
    	fs/exec.c
    	include/linux/sched.h
    	kernel/sys.c
    
    Change-Id: I133806809654b6a50509f98284807ccb560c061c

--- fs/exec.c
+++ fs/exec.c
@@ -1259,7 +1259,7 @@ int check_unsafe_exec(struct linux_binprm *bprm)
 	 * This isn't strictly necessary, but it makes it harder for LSMs to
 	 * mess up.
 	 */
-	if (current->no_new_privs)
+	if (task_no_new_privs(current))
 		bprm->unsafe |= LSM_UNSAFE_NO_NEW_PRIVS;
 
 	n_fs = 1;
@@ -1297,7 +1297,7 @@ static void bprm_fill_uid(struct linux_binprm *bprm)
 	bprm->cred->egid = current_egid();
 
 	if (bprm->file->f_path.mnt->mnt_flags & MNT_NOSUID &&
-                current->no_new_privs)
+                task_no_new_privs(current))
 		return;
 
 	inode = bprm->file->f_path.dentry->d_inode;
