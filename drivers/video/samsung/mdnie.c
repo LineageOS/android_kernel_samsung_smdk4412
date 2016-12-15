09c704a98ad581ad988ce19c0b88b7992f99a1f6
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-12-15 21:23:30 +0100

    video: samsung: mdnie: Expose no. of display modes
    CMHW uses this to determine the display modes supported by the device.
    
    Change-Id: I32bdd71f1f336967d41ec4b36f8620ce36cace4b

--- drivers/video/samsung/mdnie.c
+++ drivers/video/samsung/mdnie.c
@@ -533,6 +533,11 @@ static ssize_t mode_store(struct device *dev,
 	return count;
 }
 
+static ssize_t mode_max_show(struct device *dev,
+		struct device_attribute *attr, char *buf)
+{
+	return snprintf(buf, PAGE_SIZE, "%u\n", MODE_MAX);
+}
 
 static ssize_t scenario_show(struct device *dev,
 		struct device_attribute *attr, char *buf)
@@ -901,6 +906,7 @@ static ssize_t color_correct_show(struct device *dev,
 
 static struct device_attribute mdnie_attributes[] = {
 	__ATTR(mode, 0664, mode_show, mode_store),
+	__ATTR(mode_max, 0664, mode_max_show, NULL),
 	__ATTR(scenario, 0664, scenario_show, scenario_store),
 #if defined(CONFIG_FB_MDNIE_PWM)
 	__ATTR(cabc, 0664, cabc_show, cabc_store),
