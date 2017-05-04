b8ebcdedb8b2e774eadcd20304d0a3367017353f
Dominggoes Isakh <drjisakh@gmail.com>
2017-05-04 18:15:08 +0200

    cypress: BLN: Add screen touch support for Atmel MXT224
    Change-Id: I262f3381f2931fbb5e928bc6d161eae2b26a9b28

--- drivers/input/keyboard/cypress/cypress-touchkey.c
+++ drivers/input/keyboard/cypress/cypress-touchkey.c
@@ -198,6 +198,9 @@ static bool blnww = false;
 static int led_fadein = 0, led_fadeout = 0, led_fade_on = 0;
 static int led_on_touch = 1;
 
+#ifdef CONFIG_TOUCHSCREEN_ATMEL_MXT224_U1
+extern void (*mxt224_touch_cb)(void);
+#endif
 #endif
 
 MODULE_DEVICE_TABLE(i2c, sec_touchkey_id);
@@ -2637,6 +2640,10 @@ static int i2c_touchkey_probe(struct i2c_client *client,
 	reset_breathing_steps();
         /* wake lock for BLN */
         wake_lock_init(&bln_wake_lock, WAKE_LOCK_SUSPEND, "bln_wake_lock");
+
+#ifdef CONFIG_TOUCHSCREEN_ATMEL_MXT224_U1
+	mxt224_touch_cb = cypress_notify_touch;
+#endif
 #endif
 	return 0;
 }
