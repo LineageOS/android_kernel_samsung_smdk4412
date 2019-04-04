4e8ee0801384807c9a266ffd9354f99ec98b6c27
Dominggoes Isakh <drjisakh@gmail.com>
2019-04-04 22:30:14 +0200

    Revert "ext4: use kzalloc in ext4_kzalloc()"
    This reverts commit 6c3ca6aaf54b79586199dd93dd390de18e38dddc.

--- fs/ext4/super.c
+++ fs/ext4/super.c
@@ -126,7 +126,7 @@ void *ext4_kvzalloc(size_t size, gfp_t flags)
 {
 	void *ret;
 
-	ret = kzalloc(size, flags);
+	ret = kmalloc(size, flags);
 	if (!ret)
 		ret = __vmalloc(size, flags | __GFP_ZERO, PAGE_KERNEL);
 	return ret;
