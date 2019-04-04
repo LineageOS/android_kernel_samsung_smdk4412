ad9441a86d3f47d94da0a9fa8eea5bb55e4f0c2f
Dominggoes Isakh <drjisakh@gmail.com>
2019-04-04 22:28:23 +0200

    Revert "ext4: fix trim length underflow with small trim length"
    This reverts commit 4c084029dd0047e92af36d208424498d690aa275.

--- fs/ext4/mballoc.c
+++ fs/ext4/mballoc.c
@@ -4938,8 +4938,6 @@ int ext4_trim_fs(struct super_block *sb, struct fstrim_range *range)
 
 	if (unlikely(minlen > EXT4_CLUSTERS_PER_GROUP(sb)))
 		return -EINVAL;
-	if (start + len <= first_data_blk)
-		goto out;
 	if (start < first_data_blk) {
 		len -= first_data_blk - start;
 		start = first_data_blk;
@@ -4988,6 +4986,5 @@ int ext4_trim_fs(struct super_block *sb, struct fstrim_range *range)
 	}
 	range->len = trimmed * sb->s_blocksize;
 
-out:
 	return ret;
 }
